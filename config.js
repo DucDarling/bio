2c6b00fddffb77b092eb7ffb711183fefd12b736
DucDarling <ducdaika1507@gmail.com>
2025-09-03 10:57:47 +0700

    Create config.js

---
+++ config.js
@@ -0,0 +1,5 @@
+window.APP_CONFIG = {
+  GIST_ID: "8d901e2dc4daf9f58b0e230cbcd05105",
+  GIST_FILE: "key.json",
+  GITHUB_TOKEN: "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"   // token classic (scope: gist)
+};
